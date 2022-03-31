287d16729a1d7c9e9c70d7c8be1d760dbf707527
thongphamit <102723392+thongphamit@users.noreply.github.com>
2022-03-31 20:25:01 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,25 @@
+
+function data(){
+           alert('quý khách vui lòng tuy cập trên máy tính .... mở nhạc ở dưới để nghe nhé ..!!!')
+
+
+ 
+ var button2 = document.getElementById('item');
+ var div = document.getElementById('chat');
+  
+
+ 
+
+ button2.onclick = function(){
+     div.innerHTML ='♛ PHAMTHONG.IT Kính chúc quý khách thật nhiều sức khỏe.quý khách vui lòng nhắn tin vào zalo : 0345922756 để làm việc trực tiếp ..... Cảm ơn quý khách !!! ▬▬ι═══════ﺤ' ;
+     alert('Dịch vụ đã sẵn sàng...Qúy khách vui lọng kéo xuống phần nội dung');
+     
+    };
+
+ 
+
+    
+    
+
+
+};
